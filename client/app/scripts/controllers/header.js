e55c3d951fd2c7b1a8eff32f9343967f8a2661f1
Tyler Doster <tjdfalcon@gmail.com>
2014-11-05 16:35:03 -0800

    fix setting active class on nav pills

---
+++ client/app/scripts/controllers/header.js
@@ -0,0 +1,8 @@
+'use strict';
+
+angular.module('clientApp')
+  .controller('HeaderCtrl', function ($scope, $location) {
+        $scope.isActive = function (location) {
+          return location === $location.path()
+        }
+  });
