efbddb4d95ff65e34eec2a753940c06a80e7a78e
DJ Kaai <debert@zagmail.gonzaga.edu>
2014-11-12 17:02:44 -0800

    Ordered buildings by name.

--- server/controllers/site.js
+++ server/controllers/site.js
@@ -1,5 +1,5 @@
 exports.getBuildings = function(req, res){
-    connection.query('SELECT name FROM building_data', function(err, rows){
+    connection.query('SELECT name FROM building_data ORDER BY name', function(err, rows){
         if(err){
             throw err;
         }
