96fbdacb420547ee761dcedc0ccd5152036f6e3d
DJ Kaai <debert@zagmail.gonzaga.edu>
2014-11-12 16:51:50 -0800

    Displays query results to /api/buildings

--- server/controllers/site.js
+++ server/controllers/site.js
@@ -1,14 +1,10 @@
 exports.getBuildings = function(req, res){
-    connection.connect();
     connection.query('SELECT name FROM building_data', function(err, rows){
         if(err){
-            console.log(err);
             throw err;
         }
         else {
-            console.log('success');
             res.send(rows);
         }
     });
-    connection.end();
 };
