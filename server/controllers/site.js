f378b81317f2ef86dc9f5e1113bbeaa7c431b365
DJ Kaai <debert@zagmail.gonzaga.edu>
2014-11-17 15:35:57 -0800

    Added methods to get each resource type given a building.  This is in site.js.

--- server/controllers/site.js
+++ server/controllers/site.js
@@ -8,3 +8,37 @@ exports.getBuildings = function(req, res){
         }
     });
 };
+
+exports.getWater = function(req, res, building){
+    connection.query("SELECT water FROM building_data WHERE name = '" + building + "' ORDER BY date", function(err, rows){
+        if(err){
+            throw err;
+        }
+        else {
+            res.send(rows);
+        }
+    });
+};
+
+exports.getElectricity = function(req, res, building){
+    connection.query("SELECT electricity FROM building_data WHERE name = '" + building + "' ORDER BY date", function(err, rows){
+        if(err){
+            throw err;
+        }
+        else {
+            res.send(rows);
+        }
+    });
+};
+
+exports.getGas = function(req, res, building){
+    connection.query("SELECT gas FROM building_data WHERE name = '" + building + "' ORDER BY date", function(err, rows){
+        if(err){
+            throw err;
+        }
+        else {
+            res.send(rows);
+        }
+    });
+};
+
