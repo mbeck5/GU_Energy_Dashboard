af3ed9eab66b01972064e254c8b6f8ecd2559093
DCorisis <dcorisis@zagmail.gonzaga.edu>
2014-11-19 17:51:58 -0800

    got version control sorted out

--- client/app/scripts/controllers/buildingSelector.js
+++ client/app/scripts/controllers/buildingSelector.js
@@ -2,27 +2,30 @@
 
 angular.module('clientApp')
   .controller('BuildingSelectorCtrl', function ($scope, buildingSvc) {
-        var buildings = [];
-        $scope.searchInput = '';
-        $scope.filteredBuildings = [];
+    var buildings = [];
+    $scope.searchInput = '';
+    $scope.filteredBuildings = [];
 
-        //unpack promise returned from rest call
-        buildingSvc.getBuildings().then(function (data) {
-          buildings = data;
-          $scope.filteredBuildings = buildings;
-        });
+    //unpack promise returned from rest call
+    buildingSvc.getBuildings().then(function (data) {
+      buildings = data;
+      $scope.filteredBuildings = buildings;
+    });
 
-        //filters based on search input
-        $scope.filterBuildings = function() {
-            $scope.filteredBuildings = buildings.filter(filterBuildings);
-        };
+    var buildingScrollHeight = $(window).height() * .65;
+    $('.scroll').css({'height': buildingScrollHeight + 'px'});
 
-        //when clicking on building
-        $scope.selectBuilding = function (index) {
-            buildingSvc.setSelectedBuilding($scope.filteredBuildings[index]);
-        };
+    //filters based on search input
+    $scope.filterBuildings = function () {
+      $scope.filteredBuildings = buildings.filter(filterBuildings);
+    };
 
-        function filterBuildings(element) {
-            return element.name.toLowerCase().indexOf($scope.searchInput.toLowerCase().trim()) > -1;
-        }
+    //when clicking on building
+    $scope.selectBuilding = function (index) {
+      buildingSvc.setSelectedBuilding($scope.filteredBuildings[index]);
+    };
+
+    function filterBuildings(element) {
+      return element.name.toLowerCase().indexOf($scope.searchInput.toLowerCase().trim()) > -1;
+    }
   });
