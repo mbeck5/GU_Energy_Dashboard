be347e13ba9d515f60d5437511f4d8c97d47ecfc
DCorisis <dcorisis@zagmail.gonzaga.edu>
2014-11-23 00:46:13 -0800

    tips work

--- client/app/scripts/controllers/buildingSelector.js
+++ client/app/scripts/controllers/buildingSelector.js
@@ -3,8 +3,27 @@
 angular.module('clientApp')
   .controller('BuildingSelectorCtrl', function ($scope, buildingSvc) {
     var buildings = [];
+
+    var electricityTips = ["Etip1", "Etip2", "Etip3"];
+    var gasTips = ["Gtip1", "Gtip2", "Gtip3"];
+    var waterTips = ["Wtip1", "Wtip2", "Wtip3"];
+
     $scope.searchInput = '';
     $scope.filteredBuildings = [];
+
+    jQuery(document).ready(function() {
+      jQuery('.tabs .tab-links a').on('click', function(e)  {
+        var currentAttrValue = jQuery(this).attr('href');
+
+        // Show/Hide Tabs
+        jQuery('.tabs ' + currentAttrValue).show().siblings().hide();
+
+        // Change/remove current tab to active
+        jQuery(this).parent('li').addClass('active').siblings().removeClass('active');
+
+        e.preventDefault();
+      });
+    });
 
     //unpack promise returned from rest call
     buildingSvc.getBuildings().then(function (data) {
@@ -15,10 +34,11 @@ angular.module('clientApp')
     var windowHeight = $(window).height();
     var windowWidth = $(window).width();
 
-    var buildingScrollHeight = $(window).height() * .65;
+    var buildingScrollHeight = $(window).height() * .55;
     $('.scroll').css({'height': buildingScrollHeight + 'px'});
 
-    $('.tipText').css({'width': windowWidth * .50 + 'px'});
+    var tipTextWidth = $(window).width() * .25;
+    $('.tipText').css({'width': tipTextWidth  + 'px'});
 
     //filters based on search input
     $scope.filterBuildings = function () {
