bbba70f27807641c6c37216d9f486c5a54bf9cef
Tyler Doster <tjdfalcon@gmail.com>
2014-12-03 14:38:58 -0800

    swich to daily data

--- server/controllers/site.js
+++ server/controllers/site.js
@@ -10,8 +10,8 @@ exports.getBuildings = function(req, res){
 };
 
 exports.getResources = function(req, res){
-    connection.query("SELECT meters_mly_data.trend_date as date, meters_mly_data.consumption, meters.meter_type_id as meterTypeId" +
-    " FROM meters_mly_data INNER JOIN meters ON meters.meter_id = meters_mly_data.meter_id" +
+    connection.query("SELECT meters_dly_data.trend_date as date, meters_dly_data.consumption, meters.meter_type_id as meterTypeId" +
+    " FROM meters_dly_data INNER JOIN meters ON meters.meter_id = meters_dly_data.meter_id" +
     " WHERE meters.meter_name LIKE '%" + req.param("building") + "%' ORDER BY date", function(err, rows){
         if(err){
             throw err;
