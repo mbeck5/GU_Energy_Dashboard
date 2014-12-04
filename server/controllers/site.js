8ca55d18eec93d803c3f3e7d56828bfbd7f4edce
DJ Kaai <debert@zagmail.gonzaga.edu>
2014-12-03 17:52:43 -0800

    Changed the query that gets the resource data.  This query depends on the  building id and I don't know how to get the id into the query itself.  I believe in you Tyler.

--- server/controllers/site.js
+++ server/controllers/site.js
@@ -10,9 +10,26 @@ exports.getBuildings = function(req, res){
 };
 
 exports.getResources = function(req, res){
-    connection.query("SELECT meters_mly_data.trend_date as date, meters_mly_data.consumption, meters.meter_type_id as meterTypeId" +
-    " FROM meters_mly_data INNER JOIN meters ON meters.meter_id = meters_mly_data.meter_id" +
-    " WHERE meters.meter_name LIKE '%" + req.param("building") + "%' ORDER BY date", function(err, rows){
+    queryString = "SELECT meters_dly_data.trend_date as date, meters_dly_data.consumption, meters.meter_type_id as meterTypeId " +
+                    "FROM meters_dly_data " +
+                    "JOIN meters ON meters_dly_data.METER_ID=meters.METER_ID " +
+                    "WHERE meter_id IN (SELECT METER_ID " +
+                                        "FROM erb_tree " +
+                                        "WHERE PARENT_NODE_ID IN (SELECT NODE_ID " +
+                                                                    "FROM erb_tree " +
+                                                                    "WHERE BUILDING_ID = " + req.param("id") + ") ORDER BY date;"
+
+    /*connection.query("SELECT meters_dly_data.trend_date as date, meters_dly_data.consumption, meters.meter_type_id as meterTypeId" +
+     " FROM meters_dly_data INNER JOIN meters ON meters.meter_id = meters_dly_data.meter_id" +
+     " WHERE meters.meter_name LIKE '%" + req.param("building") + "%' ORDER BY date", function(err, rows){
+     if(err){
+     throw err;
+     }
+     else {
+     res.send(rows);
+     }
+     });*/
+    connection.query(queryString, function(err, rows){
         if(err){
             throw err;
         }
