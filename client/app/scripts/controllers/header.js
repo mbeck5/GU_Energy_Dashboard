7d1a539e68d26b43afe64d57e719c3130b0dc1d3
Tyler Doster <tjdfalcon@gmail.com>
2015-01-15 20:52:31 -0800

    use angular ui directives for navbar

--- client/app/scripts/controllers/header.js
+++ client/app/scripts/controllers/header.js
@@ -2,6 +2,8 @@
 
 angular.module('clientApp')
   .controller('HeaderCtrl', function ($scope, $location) {
+        $scope.navbarCollapsed = true;
+
         $scope.isActive = function (location) {
           return location === $location.path();
         };
