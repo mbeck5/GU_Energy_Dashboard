6b70e3ea4e5c75d37abdc4c14ea44c646cf1592c
DJ Kaai <debert@zagmail.gonzaga.edu>
2015-01-21 11:51:12 -0800

    Made query to get resource totals by building type.

--- server/controllers/site.js
+++ server/controllers/site.js
@@ -46,6 +46,29 @@ exports.getResources = function(req, res){
     });
 };
 
+exports.getResourcesByType = function(req, res){
+    queryString = "SELECT sum(CONSUMPTION) as Total, b.BUILDING_TYPE_ID " +
+                    "FROM erb_tree e, " +
+                        "(SELECT * " +
+                        "FROM (SELECT METER_ID, CONSUMPTION " +
+                                "FROM meters_dly_data " +
+                                "ORDER BY TREND_DATE DESC) as most_recent_entries " +
+                        "JOIN erb_tree ON most_recent_entries.METER_ID = erb_tree.METER_ID " +
+                        "WHERE METER_TYPE_ID= " + req.param("meterType") + " " +
+                        "GROUP BY most_recent_entries.METER_ID) as t" +
+                    "WHERE e.NODE_ID = t.PARENT_NODE_ID AND b.BUILDING_ID = e.BUILDING_ID " +
+                    "GROUP BY b.BUILDING_TYPE_ID"
+
+    connection.query(queryString, function(err, rows){
+        if(err){
+            throw err;
+        }
+        else {
+            res.send(rows);
+        }
+    });
+}
+
 exports.getResourcesFromName = function(req, res) {
     queryString = "SELECT meters_dly_data.trend_date as date, meters_dly_data.consumption " +
                     "FROM meters_dly_data " +
