019adc0bb0558bf52453d05857e6c4b1f80743c8
Tyler Doster <tjdfalcon@gmail.com>
2015-01-25 21:41:45 -0800

    this is the most confusing crap

--- client/test/displayCtrlSpec.js
+++ client/test/displayCtrlSpec.js
@@ -6,19 +6,34 @@ describe('controller: buildingDisplay', function () {
   beforeEach(module('clientApp'));
 
   var scope,
-    mockBuildingDisplayCtrl;
+    buildingDisplayCtrl,
+    deferred,
+    mockBuildingSvc,
+    data = [];
 
-  beforeEach(inject(function ($controller, $rootScope) {
+  beforeEach(inject(function ($rootScope, $controller, $q) {
     scope = $rootScope.$new();
+    mockBuildingSvc = {
+      getSelectedBuilding: function() {
+        return 'Alliance';
+      },
+      getBuildingData: function() {
+        deferred = $q.defer();
+        return deferred.promise;
+      }
+    };
 
-    mockBuildingDisplayCtrl = $controller('BuildingDisplayCtrl', {
-      $scope: scope
+    buildingDisplayCtrl = $controller('BuildingDisplayCtrl', {
+      $scope: scope,
+      buildingSvc: mockBuildingSvc
     });
   }));
 
   it('should set correct axis label', function () {
     var resource = 3;
     scope.selectResource(resource);
+    deferred.resolve(data);
+    scope.$root.$digest();
     expect(scope.options.chart.yAxis.axisLabel).toBe('Gas');
   });
 });
