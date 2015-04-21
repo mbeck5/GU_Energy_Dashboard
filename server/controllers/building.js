2954e53fcaa402ef603cf1521baeee09a562ff52
DJ Kaai <debert@zagmail.gonzaga.edu>
2015-04-21 16:54:21 -0700

    Added standard deviation to the queries themselves, so we don't need to use the standard deviation service (sorry Tyler :C ).  Also made a fix to the queries to actually get the correct values in the deployed database.

--- server/controllers/building.js
+++ server/controllers/building.js
@@ -47,18 +47,32 @@ exports.getResources = function(req, res){
         startDate = moment(startDate).format("YYYY-MM-DD HH:mm:ss");
         endDate = moment(endDate).format("YYYY-MM-DD HH:mm:ss");
     }
-    var queryString = "SELECT building_name as name, " + tableName + ".trend_date as date, SUM(" + tableName + ".consumption) as consumption " +
+    var queryString = "SELECT building_name as name, " + tableName + ".trend_date as date, SUM(DISTINCT " + tableName + ".consumption) as consumption " +
                         "FROM building, building_meters, meters, " + tableName + " " +
-                        "WHERE building_meters.meter_id = meters.meter_id AND " + tableName + ".meter_id = meters.meter_id AND trend_date >= '" + startDate +
-                            "' AND trend_date <= '" + endDate + "' AND building_meters.building_id = " + req.query.building +
-                            " AND meter_type_id = " + req.query.meterType + " AND building.building_id = building_meters.building_id GROUP BY date;";
+                        "WHERE building_meters.meter_id = meters.meter_id AND " + tableName + ".meter_id = meters.meter_id AND trend_date >= '" + startDate + "' AND trend_date <= '" + endDate + "' AND building_meters.building_id = " + req.query.building + " AND meter_type_id = " + req.query.meterType + " AND building.building_id = building_meters.building_id " +
+                        "GROUP BY date " +
+                        "HAVING consumption > " +
+                            "(SELECT AVG(consumption) - (3 * STDDEV(consumption)) " +
+                            "FROM (" +
+                                "SELECT SUM(DISTINCT " + tableName + ".consumption) as consumption " +
+                                "FROM building, building_meters, meters, " + tableName + " " +
+                                "WHERE building_meters.meter_id = meters.meter_id AND " + tableName + ".meter_id = meters.meter_id AND trend_date >= '" + startDate + "' AND trend_date <= '" + endDate + "' AND building_meters.building_id = " + req.query.building + " AND meter_type_id = " + req.query.meterType + " AND building.building_id = building_meters.building_id " +
+                                "GROUP BY trend_date) as t) AND " +
+                        "consumption < " +
+                            "(SELECT AVG(consumption) + (3 * STDDEV(consumption)) " +
+                            "FROM (" +
+                                "SELECT SUM(DISTINCT " + tableName + ".consumption) as consumption " +
+                                "FROM building, building_meters, meters, " + tableName + " " +
+                                "WHERE building_meters.meter_id = meters.meter_id AND " + tableName + ".meter_id = meters.meter_id AND trend_date >= '" + startDate + "' AND trend_date <= '" + endDate + "' AND building_meters.building_id = " + req.query.building + " AND meter_type_id = " + req.query.meterType + " AND building.building_id = building_meters.building_id " +
+                                "GROUP BY trend_date) as t);";
 
     connection.query(queryString, function(err, rows){
         if(err){
             throw err;
         }
         else {
-            res.send(stdDev.standardDeviationFilter(rows));
+            //res.send(stdDev.standardDeviationFilter(rows));
+            res.send(rows);
         }
     });
 };
@@ -83,24 +97,32 @@ exports.getResourcesFromName = function(req, res) {
         startDate = moment(startDate).format("YYYY-MM-DD HH:mm:ss");
         endDate = moment(endDate).format("YYYY-MM-DD HH:mm:ss");
     }
-    var queryString = "SELECT '" + req.query.building + "' as name, " + tableName + ".trend_date as date, SUM(" + tableName + ".consumption) as consumption " +
-                        "FROM " + tableName + " " +
-                        "JOIN meters ON " + tableName + ".METER_ID=meters.METER_ID " +
-                        "WHERE meter_type_id = " + req.query.meterType + " AND " + tableName + ".trend_date >= '" + startDate + "' AND " + tableName + ".trend_date <= '" + endDate + "' AND meters.meter_id IN " +
-                            "(SELECT METER_ID " +
-                                "FROM erb_tree " +
-                                "WHERE PARENT_NODE_ID IN (SELECT NODE_ID " +
-                                                            "FROM erb_tree " +
-                                                            "WHERE BUILDING_ID IN (SELECT building_id " +
-                                                                                    "FROM building " +
-                                                                                    "WHERE building_name = '" + req.query.building + "'))) GROUP BY date;";
+    var queryString = "SELECT building_name as name, " + tableName + ".trend_date as date, SUM(DISTINCT " + tableName + ".consumption) as consumption " +
+                        "FROM building, building_meters, meters, " + tableName + " " +
+                        "WHERE building_meters.meter_id = meters.meter_id AND " + tableName + ".meter_id = meters.meter_id AND trend_date >= '" + startDate + "' AND trend_date <= '" + endDate + "' AND building.building_name = '" + req.query.building + "' AND meter_type_id = " + req.query.meterType + " AND building.building_id = building_meters.building_id " +
+                        "GROUP BY date " +
+                        "HAVING consumption > " +
+                            "(SELECT AVG(consumption) - (3 * STDDEV(consumption)) " +
+                            "FROM (" +
+                                "SELECT SUM(DISTINCT " + tableName + ".consumption) as consumption " +
+                                "FROM building, building_meters, meters, " + tableName + " " +
+                                "WHERE building_meters.meter_id = meters.meter_id AND " + tableName + ".meter_id = meters.meter_id AND trend_date >= '" + startDate + "' AND trend_date <= '" + endDate + "' AND building.building_name = '" + req.query.building + "' AND meter_type_id = " + req.query.meterType + " AND building.building_id = building_meters.building_id " +
+                                "GROUP BY trend_date) as t) AND " +
+                        "consumption < " +
+                            "(SELECT AVG(consumption) + (3 * STDDEV(consumption)) " +
+                            "FROM (" +
+                                "SELECT SUM(DISTINCT " + tableName + ".consumption) as consumption " +
+                                "FROM building, building_meters, meters, " + tableName + " " +
+                                "WHERE building_meters.meter_id = meters.meter_id AND " + tableName + ".meter_id = meters.meter_id AND trend_date >= '" + startDate + "' AND trend_date <= '" + endDate + "' AND building.building_name = '" + req.query.building + "' AND meter_type_id = " + req.query.meterType + " AND building.building_id = building_meters.building_id " +
+                                "GROUP BY trend_date) as t);";
 
     connection.query(queryString, function(err, rows){
         if(err){
             throw err;
         }
         else {
-            res.send(stdDev.standardDeviationFilter(rows));
+            //res.send(stdDev.standardDeviationFilter(rows));
+            res.send(rows)
         }
     });
 };
@@ -122,7 +144,8 @@ exports.getResourcesByType = function(req, res){
                         "FROM building, building_meters, meters, meters_dly_data " +
                         "WHERE building_meters.meter_id = meters.meter_id AND meters_dly_data.meter_id = meters.meter_id AND trend_date = '" + date +
                         "' AND building_meters.building_id IN (SELECT building.building_id FROM building WHERE building_id != 1) " +
-                        " AND meter_type_id = " + req.query.meterType + " AND building.building_id = building_meters.building_id GROUP BY building.building_id) as t, building_type " +
+                        " AND meter_type_id = " + req.query.meterType + " AND building.building_id = building_meters.building_id " +
+                            "GROUP BY building.building_id HAVING consumption > AVG(consumption) - (3 * STDDEV(consumption)) AND consumption < AVG(consumption) + (3 * STDDEV(consumption))) as t, building_type " +
                     "WHERE t.building_type_id = building_type.building_type_id AND building_type.building_type_id != 1 GROUP BY t.building_type_id;";
 
     connection.query(queryString, function(err, rows){
