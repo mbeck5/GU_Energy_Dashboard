6c4ba715ed27b3c34fd1d5c39f245f47f1fc0f3e
Tyler Doster <tjdfalcon@gmail.com>
2015-04-24 10:52:51 -0700

    add documentation to our controllers

--- client/app/scripts/controllers/header.js
+++ client/app/scripts/controllers/header.js
@@ -2,15 +2,17 @@
 
 angular.module('clientApp')
   .controller('HeaderCtrl', function ($scope, $location) {
-        $scope.navbarCollapsed = true;
+    $scope.navbarCollapsed = true;
 
-        $scope.isActive = function (location) {
-          return location === $location.path();
-        };
+    //returns which page is current selected
+    $scope.isActive = function (location) {
+      return location === $location.path();
+    };
 
-        $scope.toggleCollapse = function() {
-          //don't toggle if large screen
-          if ($(document).width() < 992)
-            $scope.navbarCollapsed = !$scope.navbarCollapsed;
-        };
+    //toggles navbar
+    $scope.toggleCollapse = function() {
+      //don't toggle if large screen
+      if ($(document).width() < 992)
+        $scope.navbarCollapsed = !$scope.navbarCollapsed;
+    };
   });
